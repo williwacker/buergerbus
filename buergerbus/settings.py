81bade6b5fe25cab6324c886578c17a370291e1a
Werner Kühn <werner_kuehn@gmx.de>
2019-12-16 17:01:55 +0100

    12/16

--- buergerbus/settings.py
+++ buergerbus/settings.py
@@ -162,6 +162,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = config('STATIC_URL', default='/static/')
+STATIC_ROOT = config('STATIC_ROOT', default='')
 
 STATICFILES_DIRS = [
 	os.path.join(BASE_DIR, "static"),
