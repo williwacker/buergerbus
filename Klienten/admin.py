adf648d421d01f3259d7816d8444d8e641f6dda1
Werner Kühn <werner_kuehn@gmx.de>
2020-02-12 13:12:07 +0100

    allow admin update of Ort

--- Klienten/admin.py
+++ Klienten/admin.py
@@ -69,13 +69,13 @@ class StrassenAdmin(admin.ModelAdmin):
 	ordering = ('ort','strasse')
 	list_filter = ('ort',)
 	search_fields = ('strasse', 'ort__ort')
-
+	'''
 	def get_readonly_fields(self, request, obj=None):
 		if obj:
 			return ["ort"]
 		else:
 			return []
-
+	'''
 admin.site.register(Klienten, KlientenAdmin)
 admin.site.register(Orte, OrteAdmin)
 admin.site.register(Strassen, StrassenAdmin)
